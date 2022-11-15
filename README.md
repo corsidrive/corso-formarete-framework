f6cd25041cb0002a5b9677b623a9096009d502ab
Sarasso Roberto <sarassoroberto@gmail.com>
2022-11-15 12:27:33 +0100

    NODELIST e metodo forEach e le varibili

--- README.md
+++ README.md
@@ -9,6 +9,10 @@
 - [documentQuerySelector](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector)
 - [CSS Selector](https://www.w3schools.com/csSref/css_selectors.php) 
 
+
+- [NodeList](https://developer.mozilla.org/en-US/docs/Web/API/NodeList?retiredLocale=it)
+    - [Callback e .forEach](https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach)
+
 ## link utili
 
 - [Freecodecamp](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#basic-javascript)
---
+++ basi_js/js/le_variabili.js
@@ -0,0 +1,31 @@
+// ECMASCRIPT prima delll 2015 | ES6
+// DICHIARAZIONE
+var indirizzo 
+// ASSEGNAZIONE
+indirizzo = "via qui"
+
+var indirizzo = "via la"
+
+console.log("---------")
+
+
+// $oggetto->proprieta
+
+// non può essere RIDICHIARATA
+// non può essere RIASSEGNATA
+const nome = "Roberto"
+// nome = "luca" // DA ERRORE
+
+// non può essere RIDICHIARATA
+let cognome = "Rossi"
+cognome = "Verdi"
+
+// console.log(cognome)
+// bracket scope // scope legato alle parentesi
+{
+    let anni = 12
+    console.log(anni)
+}
+
+// 
+
