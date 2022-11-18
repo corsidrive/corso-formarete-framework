e5665cee02604a176d5d07c31f55e9b31369bba2
Sarasso Roberto <sarassoroberto@gmail.com>
2022-11-18 12:35:48 +0100

    array every

--- basi_js/js/array_methods.js
+++ basi_js/js/array_methods.js
@@ -5,12 +5,12 @@ const Todoist = [
 
     {
         label:"Comprare il latte",
-        done:false,
+        done:true,
         tags: ['urgente','casa']
     },
     {
         label:"Comprare Farina",
-        done:false,
+        done:true,
         tags: ['casa']
     },
     {
@@ -26,8 +26,32 @@ function urgente(todo,index,all) {
     return res
 }
 
+function findTag(search){
+        return function urgente(todo) {      
+            const res = todo.tags.includes(search)
+            return res
+        }
+}
+
 const res = Todoist.filter(urgente)
 
 // console.log(res);
 
-console.log(Todos.findTag("casa"));
+console.log(Todoist.filter(findTag("casa")));
+
+
+
+
+
+function allDone(todo){
+    return todo.done == true
+}
+
+console.log("ho fatto tutto ?", Todoist.every(allDone))
+console.log("ho fatto tutto per la casa ?", Todoist.filter(findTag('casa')).every(allDone))
+
+// Hofatto tutto ?
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
