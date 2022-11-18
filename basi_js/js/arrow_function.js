a268352404016aaedff7ab56991267d09328cc61
Sarasso Roberto <sarassoroberto@gmail.com>
2022-11-18 12:58:36 +0100

    arraw function

---
+++ basi_js/js/arrow_function.js
@@ -0,0 +1,23 @@
+console.clear()
+const colori =  ['red','green','yellow']
+
+const res = colori.find(function(colore){
+    return colore == 'red'
+})
+
+
+const res2 = colori.find((colore,index) => {
+    return colore == 'red'
+})
+
+const res3 = colori.find( colore => {
+    return colore == 'red'
+})
+
+
+const res4 = colori.find( colore => colore == 'red')
+
+const res5 = colori.find( (colore,index) => colore == 'red' )
+
+
+console.log("ho trovato",res,colori)
