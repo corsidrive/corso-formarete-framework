ea9298e103b5e55cb280026e7c61e7b0ec334610
Sarasso Roberto <sarassoroberto@gmail.com>
2022-11-23 13:00:41 +0100

    rendering item base ok

--- todo/todo_rendering_module.js
+++ todo/todo_rendering_module.js
@@ -1,8 +1,17 @@
 // template literal 
 function RenderingListItem(todo) {
+    // Ternary operator
+    const _checked = todo.done ? 'checked': ''
+    const _done = todo.done ? 'done': ''
+    
+    console.log(_checked,todo.done)
+    // checked
     return `<li class="list-group-item">
-                <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-                <label class="form-check-label" for="firstCheckbox">
+            <input class="form-check-input me-1" ${_checked} 
+                type="checkbox" 
+                id="${todo.id}"
+                >
+                <label class="form-check-label ${_done}" for="${todo.id}">
                     ${todo.label}
                 </label>
             </li>`
