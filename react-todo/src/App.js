e10837345e1db206ae31adf511d126e3dff0c63d
Roberto Sarasso <sarassoroberto@gmail.com>
2022-12-01 11:01:03 +0100

    inizio rendering list item

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,32 +1,28 @@
 import './App.css';
-
+import TodoList from './db.json'
+// const TodoList = 
+// console.log(TodoList);
 function App() {
   return (
     <div className="App">
-      <main class="container">
-        <header class="p-3 bg-secondary text-center">
+      <main className="container">
+        <header className="p-3 bg-secondary text-center">
             <h1>Todo APP</h1>
         </header>
         
-        <div class="input-group my-3">
-          <input type="text" class="form-control" placeholder="Aggiungi una cosa da fare" aria-label="Aggiungi una cosa da fare" aria-describedby="bottone-aggiungi"/>
-          <button class="btn btn-primary" type="button" id="bott-aggiungi">aggiungi</button>
+        <div className="input-group my-3">
+          <input type="text" className="form-control" placeholder="Aggiungi una cosa da fare" aria-label="Aggiungi una cosa da fare" aria-describedby="bottone-aggiungi"/>
+          <button className="btn btn-primary" type="button" id="bott-aggiungi">aggiungi</button>
         </div>
         
-        <ul class="list-group">
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/>
-              <label class="form-check-label" for="firstCheckbox">First checkbox</label>
+        <ul className="list-group">
+
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/>
+              <label className="form-check-label" htmlFor="firstCheckbox">First checkbox</label>
             </li>
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckbox"/>
-              <label class="form-check-label" for="secondCheckbox">Second checkbox</label>
-            </li>
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox"/>
-              <label class="form-check-label" for="thirdCheckbox">Third checkbox</label>
-            </li>
-          </ul>
+            
+        </ul>
 
     </main>
     </div>
