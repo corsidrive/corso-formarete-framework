ba34681e23197264f9698181ab661e0ee8c4f207
Roberto Sarasso <sarassoroberto@gmail.com>
2022-12-15 17:44:31 +0100

    search

--- react-todo/src/components/search-bar.js
+++ react-todo/src/components/search-bar.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 export default function SearchBar(props) {
     const  [newLabel,setNewLabel] = useState('')
+    const  [tags,setTags] = useState('')
     const handleNewLabel = event => {
       setNewLabel(event.target.value)
       // console.log(event.target.value,newLabel)
@@ -9,7 +10,24 @@ export default function SearchBar(props) {
 
     const handleClick = () => {
         console.log("handleClick",newLabel)
-        props.onBottoneAggiungiPremuto(newLabel) 
+        props.onBottoneAggiungiPremuto(newLabel,tags) 
+    }
+
+    const deleteTag = (tagToRemove) => { 
+
+        return (event) => {
+          const tagsArray = tags.split(",")
+          const newTags = tagsArray.filter(t => t !== tagToRemove).join(",")
+  
+  
+          console.log(newTags)
+          setTags(newTags)
+        }
+
+    }
+
+    const handletags = (event) => {
+        setTags(event.target.value)
     }
 
     return (
@@ -27,7 +45,23 @@ export default function SearchBar(props) {
                  placeholder="Aggiungi una cosa da fare" 
                  aria-label="Aggiungi una cosa da fare" 
                  aria-describedby="bottone-aggiungi"/>
+
+          
           <button onClick={handleClick} className="btn btn-primary" type="button" id="bott-aggiungi">{props.buttonLabel}</button>
+        </div>
+        <div className="mb-3">
+        <input id="tags" 
+                 
+                 value={tags} 
+                 onChange={handletags}
+                 
+                 type="text" 
+                 className="form-control" 
+                 placeholder="Aggiungi una cosa da fare" 
+                 aria-label="Aggiungi una cosa da fare" 
+                 aria-describedby="bottone-aggiungi"/>
+                
+                {tags.length} - {tags.split(",").filter(i => i !='').map((i)=> (<b key={i} className="badge bg-primary me-2">{i} <span onClick={deleteTag(i)}>X</span></b>))}
         </div>
         </>
     )
